182269629c95f15726e938c2fa680dd0a931d5a3
pfuni <kurigry@gmail.com>
2026-03-03 22:27:26 -0800

    Bump version from 1.0.0 to 2.0.0

--- spotlight.user.js
+++ spotlight.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Spotlight
 // @namespace    spotlight-web-launcher
-// @version      1.0.0
+// @version      2.0.0
 // @author       pfuni
 // @updateURL    https://github.com/pfuni/spotlight/raw/refs/heads/main/spotlight.user.js
 // @downloadURL  https://github.com/pfuni/spotlight/raw/refs/heads/main/spotlight.user.js
